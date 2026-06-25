195cf73a602cd0e223b75ecf596e0aea709c67f5
Alejandro <alejandro.ze02ggmail.com>
2026-06-25 20:23:50 +0200

    v2

--- src/data/carta.ts
+++ src/data/carta.ts
@@ -3,7 +3,7 @@ import type { ICategoria } from "../interfaces/Categoria";
 export const carta_bar: ICategoria[] = [
   {
     nombre: "PARA_COMPARTIR",
-    imagen: "nachos.png",
+    imagen: "nachos.webp",
     productos: [
       {
         nombre: "ENSALADA_NOVA",
@@ -13,17 +13,16 @@ export const carta_bar: ICategoria[] = [
       {
         nombre: "TABLA_LACON_FETA",
         precio: 10,
-        nuevo: true,
       },
       {
         nombre: "SURTIDO_HEAVENS",
         descripcion: "SURTIDO_HEAVENS_DESC",
-        precio: 14,
+        precio: 16,
       },
       {
         nombre: "MINI_SURTIDO_HEAVENS",
         descripcion: "SURTIDO_HEAVENS_DESC",
-        precio: 9,
+        precio: 10,
       },
       {
         nombre: "NACHOS",
@@ -36,27 +35,21 @@ export const carta_bar: ICategoria[] = [
         precio: 11,
       },
       {
-        nombre: "NACHOS_HEAVEN",
-        descripcion: "NACHOS_HEAVEN_DESC",
+        nombre: "NACHOS_HEAVENS",
+        descripcion: "NACHOS_HEAVENS_DESC",
         precio: 12,
-      },
-      {
-        nombre: "NACHOS_BARBACOA",
-        descripcion: "NACHOS_BARBACOA_DESC",
-        precio: 12,
-        nuevo: true,
       },
     ],
   },
   {
     nombre: "HAMBURGUESAS",
     descripcion: "HB_CON_PAN_BRIOCHE",
-    imagen: "burguer.png",
+    imagen: "burguer.webp",
     productos: [
       {
         nombre: "HB_CLASICA_CON_QUESO",
         descripcion: "HB_CLASICA_CON_QUESO_DESC",
-        precio: 7.5,
+        precio: 8,
       },
       {
         nombre: "HB_POLLO",
@@ -66,55 +59,49 @@ export const carta_bar: ICategoria[] = [
       {
         nombre: "HB_HEAVENS",
         descripcion: "HB_HEAVENS_DESC",
-        precio: 9,
+        precio: 9.5,
       },
       {
         nombre: "HB_CINECOPAS",
         descripcion: "HB_CINECOPAS_DESC",
-        precio: 10,
+        precio: 11,
       },
       {
         nombre: "HB_AMERICANA",
         descripcion: "HB_AMERICANA_DESC",
-        precio: 11,
+        precio: 12,
       },
       {
         nombre: "HB_BARBACOA",
         descripcion: "HB_BARBACOA_DESC",
-        precio: 11,
+        precio: 12,
       },
       {
         nombre: "HB_SMASH_ZETA",
         descripcion: "HB_SMASH_ZETA_DESC",
         precio: 12,
       },
-      {
-        nombre: "HB_V_DOBLE",
-        descripcion: "HB_V_DOBLE_DESC",
-        precio: 12,
-        nuevo: true,
-      },
     ],
   },
   {
     nombre: "TOSTAS",
-    imagen: "tostada.png",
+    imagen: "tostada.webp",
     productos: [
       {
         nombre: "DE_LOMO",
-        precio: 4,
+        precio: 4.5,
       },
       {
         nombre: "DE_LOMO_QUESO_CURADO",
-        precio: 4.5,
+        precio: 5,
       },
       {
         nombre: "DE_SOBRASADA_QUESO",
-        precio: 4.5,
+        precio: 5,
       },
       {
         nombre: "DE_QUESO_CURADO",
-        precio: 4,
+        precio: 4.5,
       },
       {
         nombre: "DE_MOZZARELLA_TOMATE_OREGANO",
@@ -122,23 +109,19 @@ export const carta_bar: ICategoria[] = [
       },
       {
         nombre: "DE_JAMON_SERRANO",
-        precio: 4,
-      },
-      {
-        nombre: "DE_JAMOM_IBERICO",
-        precio: 7,
-      },
-      {
-        nombre: "DE_JAMON_SERRADO_QUESO_CURADO",
         precio: 4.5,
+      },
+      {
+        nombre: "DE_JAMON_IBERICO",
+        precio: 7.5,
+      },
+      {
+        nombre: "DE_JAMON_SERRANO_QUESO_CURADO",
+        precio: 5,
       },
       {
         nombre: "DE_ATUN_TOMATE_ANCHOAS",
-        precio: 4.5,
-      },
-      {
-        nombre: "DE_IDIAZABAL",
-        precio: 7,
+        precio: 5,
       },
       {
         nombre: "VITTOS",
@@ -149,80 +132,87 @@ export const carta_bar: ICategoria[] = [
   },
   {
     nombre: "SANDWICHES",
-    imagen: "sandwich.png",
+    imagen: "sandwich.webp",
     productos: [
       {
         nombre: "SW_MIXTO",
         descripcion: "SW_MIXTO_DESC",
-        precio: 3,
+        precio: 3.5,
       },
       {
         nombre: "SW_DE_POLLO",
         descripcion: "SW_DE_POLLO_DESC",
-        precio: 5,
+        precio: 5.5,
       },
       {
         nombre: "SW_METROPOLIS",
         descripcion: "SW_METROPOLIS_DESC",
-        precio: 5,
+        precio: 5.5,
       },
       {
         nombre: "SW_DE_POLLO_PLUS",
         descripcion: "SW_DE_POLLO_PLUS_DESC",
-        precio: 6,
+        precio: 6.5,
       },
       {
         nombre: "SW_HEAVENS",
         descripcion: "SW_HEAVENS_DESC",
         precio: 7,
-        nuevo: true,
       },
     ],
   },
   {
     nombre: "PERRITOS",
-    imagen: "perrito.png",
+    imagen: "perrito.webp",
     productos: [
       {
         nombre: "CLASICO",
         descripcion: "CLASICO_DESC",
-        precio: 3.5,
+        precio: 4,
       },
       {
         nombre: "HEAVENS",
         descripcion: "HEAVENS_DESC",
-        precio: 5,
+        precio: 5.5,
       },
       {
         nombre: "QUO",
         descripcion: "QUO_DESC",
-        precio: 5.5,
+        precio: 6,
       },
     ],
   },
   {
-    nombre: "GOFRES",
-    imagen: "gofre.png",
+    nombre: "POSTRES",
+    imagen: "gofre.webp",
     productos: [
       {
-        nombre: "SOLO",
+        nombre: "TARTA_OPERA",
+        precio: 7,
+      },
+      {
+        nombre: "TIRAMISU",
+        precio: 6,
+      },
+      {
+        nombre: "GOFRE",
         precio: 3,
       },
       {
-        nombre: "CON_NATA",
+        nombre: "GOFRE_NATA",
         precio: 3.5,
       },
       {
-        nombre: "CON_NOCILLA",
-        precio: 3.5,
-      },
-      {
-        nombre: "CON_NOCILLA_NATA",
+        nombre: "GOFRE_NOCILLA",
         precio: 4,
       },
       {
-        nombre: "CON_BAYLEYS",
+        nombre: "GOFRE_NOCILLA_NATA",
         precio: 4.5,
+      },
+      {
+        nombre: "GOFRE_BAYLEYS",
+        precio: 5,
       },
     ],
   },
